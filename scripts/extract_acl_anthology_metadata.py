f830d60bd36bb79ff3794a9cebe3432892738971
Zdenek Kasner <zdenek.kasner@gmail.com>
2023-08-31 16:53:46 +0200

    Add INLG pdfs

--- scripts/extract_acl_anthology_metadata.py
+++ scripts/extract_acl_anthology_metadata.py
@@ -2,12 +2,14 @@
 
 import os
 import pandas as pd
+import shutil
 
 # Define a list to store data from each paper
 data = []
 
 # Define the top-level folder containing subfolders
-top_folder = "/data/Uni/ufal/sigdial/final"  # Replace with the actual path
+conference = "inlg"
+top_folder = f"/data/Uni/ufal/sigdial/anthology/{conference}"  # Replace with the actual path
 
 # Iterate through the subfolders
 for folder_name in os.listdir(top_folder):
@@ -17,6 +19,9 @@ for folder_name in os.listdir(top_folder):
     if os.path.isdir(folder_path):
         # Extract paper_id from the folder name
         paper_id = folder_name
+
+        paper_abstract = None
+        paper_path = None
 
         # Construct the path to the metadata file
         metadata_file = os.path.join(folder_path, f"{paper_id}_metadata.txt")
@@ -35,21 +40,38 @@ for folder_name in os.listdir(top_folder):
             # Remove all newlines from the abstract
             paper_abstract = paper_abstract.replace("\n", " ")
 
-            # Append the data to the list
-            data.append([paper_id, paper_abstract])
+        # Construct the path to the metadata file
+        paper_file = os.path.join(folder_path, f"{paper_id}_Paper.pdf")
+
+        # Check if the paper file exists
+        if os.path.isfile(paper_file):
+            shutil.copy(paper_file, f"static/papers/{conference}")
+
+            paper_path = f"static/papers/{conference}/{paper_id}_Paper.pdf"
+
+        # Append the data to the list
+        data.append([paper_id, paper_abstract, paper_path])
 
 # Create a Pandas DataFrame
-df = pd.DataFrame(data, columns=["paper_id", "paper_abstract"])
+df = pd.DataFrame(data, columns=["paper_id", "abstract", "paper"])
 
 
 # Load the existing CSV file into a DataFrame
 existing_df = pd.read_csv("sitedata/papers.csv")
 
+# drop the existing columns "paper_id", "abstract", "paper" if they exist
+existing_df.drop(columns=["paper_id", "abstract", "paper"], inplace=True, errors="ignore")
+
 # Merge the existing DataFrame with the 'df' DataFrame on 'original_id' and 'paper_id'
 merged_df = existing_df.merge(df, left_on="original_id", right_on="paper_id", how="left")
 
+# Remove duplicate columns
+merged_df = merged_df.loc[:, ~merged_df.columns.duplicated()]
+
 # Rename the new column as needed (e.g., 'paper_abstract')
-merged_df.rename(columns={"paper_abstract": "abstract"}, inplace=True)
+# merged_df.rename(columns={"paper_abstract": "abstract", "paper_name": "paper"}, inplace=True)
+
+breakpoint()
 
 # Save the updated DataFrame back to the CSV file
 merged_df.to_csv("sitedata/papers.csv", index=False)
